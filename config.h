5b01654caa7efa8cab28e4ce08bb611c69806f01
ivandimitrov8080 <ivan@ivandimitrov.co.uk>
2022-05-07 23:49:29 +0000

    delete useless tags

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
